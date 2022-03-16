13d2384f7fed0f76577cd94c6c502773664414e0
Fadlyabib <91892354+Fadlyabib@users.noreply.github.com>
2022-03-16 18:59:51 +0700

    Function

---
+++ P3-Function/tugas/atas_bootstrap.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<!--
+This is a starter template page. Use this page to start your new project from
+scratch. This page gets rid of all links and provides the needed markup only.
+-->
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>SILOKER - STTNF</title>
+  <!-- Google Font: Source Sans Pro -->
+  <link rel="stylesheet"
+    href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
+  <!-- Font Awesome Icons -->
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <!-- Theme style -->
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+</head>
+
+<body class="layout-top-nav bg-light">
+  <div class="wrapper">
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-white">
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link text-primary" href="#">Home</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-primary" href="#">Pendaftaran</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-primary" href="#">Form Nilai</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link text-primary" href="#">Berita</a>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <div>
+              <form class="form-inline">
+                <div class="input-group input-group-sm pt-1">
+                  <input class="form-control form-control-navbar bg-light" type="search" placeholder="Search"
+                    aria-label="Search">
+                  <div class="input-group-append">
+                    <button class="btn btn-navbar bg-light" type="submit">
+                      <i class="fas fa-search"></i>
+                    </button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </li>
+          <li class="nav-item">
+            <div><a href="#" class="nav-link text-primary">Login</a></div>
+          </li>
+        </ul>
+      </div>
+    </nav>
+    <hr class="mt-0">
---
+++ P3-Function/tugas/konten_bootstrap.php
@@ -0,0 +1,5 @@
+<?php
+include_once('atas_bootstrap.php');
+require_once('body_bootstrap.php');
+include_once('bawah_bootstrap.php');
+?>
